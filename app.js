182aca86186bec30ecb5217c8c93a8dd5a5898a4
sebasusnik <sebasusnik@gmail.com>
2021-10-25 12:12:06 -0300

    server express, html and css

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const path = require('path')
+
+
+app.get('/', (req, res) => {res.sendFile(path.join(__dirname, 'views/index.html'))});
+app.get('/shop', (req, res) => {res.sendFile(path.join(__dirname, 'views/shop.html'))});
+app.get('/product', (req, res) => {res.sendFile(path.join(__dirname, 'views/product.html'))});
+app.get('/cart', (req, res) => {res.sendFile(path.join(__dirname, 'views/cart.html'))});
+app.get('/signup', (req, res) => {res.sendFile(path.join(__dirname, 'views/signup.html'))});
+app.get('/login', (req, res) => {res.sendFile(path.join(__dirname, 'views/login.html'))});
+
+
+app.use(express.static('public'));
+
+
+app.listen(3060, () => console.log('Servidor corriendo'));
